62b7353ae53057193deb8aba5f589a1b3692e71c
Micheal-Winn <thant.zin.winnnn@gmail.com>
2022-10-16 23:09:57 +0630

    order code

--- controller/orderController.js
+++ controller/orderController.js
@@ -6,7 +6,9 @@ const createOrder = async (req,res,next)=>
    try {
       const newOrder = req.body;
        const userId = req.user._id;
-       const order = await orderServices.createOrder(newOrder,userId)
+       newOrder.paidAt = Date.now();
+       newOrder.user = userId
+       const order = await orderServices.createOrder(newOrder)
       if(!order)throw Error('Order cannot be created')
 
        res.status(201).json({
@@ -20,6 +22,31 @@ const createOrder = async (req,res,next)=>
 
 }
 
+
+const getSingleOrder = async (req,res,next)=>
+{
+   try {
+       const orderId = req.params.id;
+       const order = await orderServices.getSingleOrder(orderId)
+       if(!order)throw Error('Order cannot be found with Id')
+       res.status(200).json({success : true,order})
+   }catch (e) {
+       res.status(400).json({success : false,message : e.message})
+   }
+
+}
+//LoggedIN user orders
+const myOrders = async (req,res,next)=>
+{
+        const userId = req.user._id;
+        const order = await orderServices.findAllOrders(userId)
+        res.status(200).json({success : true,order})
+
+
+}
+
 module.exports = {
-    createOrder
+    createOrder,
+    getSingleOrder,
+    myOrders
 }
--- services/orderServices.js
+++ services/orderServices.js
@@ -2,12 +2,23 @@ const Order = require('../model/orderModel')
 
 const createOrder = async (req)=>
 {
-    const order = new Order(
-        {req,paidAt:Date.now()
-        })
+    const order = await new Order(req)
     return order.save()
 }
 
+const getSingleOrder = async (id)=>
+{
+    const order = await Order.findById(id).populate('User','name email')
+    return order
+}
+const findAllOrders = async (id)=>
+{
+    const order = await Order.find(id)
+    return order
+}
+
 module.exports = {
-    createOrder
+    createOrder,
+    getSingleOrder,
+    findAllOrders
 }
